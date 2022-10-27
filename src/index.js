8de4e1d4f989ae19c659b9f680c4fc68621bd5a2
2ujin <suddenlydisappear200@gmail.com>
2022-10-28 00:28:39 +0900

    Feat: Global style to app css

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import TodosContextProvider from './store/todo-context';
 import './index.css';
 import App from './App';
-import GlobalStyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <TodosContextProvider>
-        <GlobalStyle />
         <div className="wrapper">
           <App />
         </div>
