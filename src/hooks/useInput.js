7f04f8a86ffc58572a55af585665d62d58f01188
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-10-28 01:43:05 +0900

    Refactor : use memoization AuthForm, UseInput

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,13 +1,16 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 const useInput = (validateValue, initValue = '') => {
   const [inputState, setInputState] = useState(initValue);
 
   const valueIsValid = validateValue(inputState);
 
-  const changeHandler = e => {
-    setInputState(e.target.value);
-  };
+  const changeHandler = useCallback(
+    e => {
+      setInputState(e.target.value);
+    },
+    [inputState]
+  );
 
   const reset = () => {
     setInputState(initValue);
