4492f4f19b3c9c8f4294b10f320360dd12485c14
triplexlab <thdbsgh3443@naver.com>
2022-10-27 16:30:17 +0900

    Revert "update API handler and TodoContext"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
-import Auth from './pages/Auth';
-import Todo from './pages/Todo';
 import './App.css';
-import { AlertModalProvider } from './store/modal-context';
 
 function App() {
   return (
     <div className="App">
-      <AlertModalProvider>
-        <Routes>
-          <Route path="/" element={<Auth />} />
-          <Route path="/todo" element={<Todo />} />
-        </Routes>
-      </AlertModalProvider>
+      <h1>test</h1>
     </div>
   );
 }
